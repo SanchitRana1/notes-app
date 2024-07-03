748aa6d3b5d5dd2f4287e79c19fdd0589b4ae1b5
Rana <sanchitrana128@gmail.com>
2024-07-04 03:15:38 +0530

    deployment info added

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const userRoutes = require("./Routes/userRoutes");
 const noteRoutes = require("./Routes/noteRoutes");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 app.use(
   bodyParser.urlencoded({
@@ -22,12 +23,24 @@ app.use(cors());
 dotenv.config();
 connectDB(); //to connect to MONGO DB
 
-app.get("/", (req, res) => {
-  res.send("API is Running");
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// ************DEPLOY***********
+const __dirname1 = path.resolve()
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname1,"/frontend/dist")))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"frontend","dist","index.html"))
+  })
+}
+else{
+  app.get("/", (req, res) => {
+    res.send("API is Running");
+  });
+}
+// ************DEPLOY***********
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
