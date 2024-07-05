9543fba5c7e30eb6fd7f87db0ad562118ec98988
Rana <sanchitrana128@gmail.com>
2024-07-05 17:21:17 +0530

    added env variables in frontend.

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,12 @@
-export const NOTES_URL = "http://localhost:5000/api/notes/"
-export const USER_URL = "http://localhost:5000/api/users/"
+const API_BASE_URL = import.meta.env.VITE_NODE_ENV === 'production' ? 'https://notez-mgfn.onrender.com/api' : 'http://localhost:5000/api';
+
+// export const NOTES_URL = "http://localhost:5000/api/notes/"
+// export const USER_URL = "http://localhost:5000/api/users/"
+// export const CLOUD_UPLOAD_URL = "https://api.cloudinary.com/v1_1/da9vcx3og/image/upload"
+// export const BG_HOME = "https://images.pexels.com/photos/1629212/pexels-photo-1629212.jpeg?auto=compress&cs=tinysrgb&w=7680&h=4320&dpr=1"
+
+export const NOTES_URL = API_BASE_URL+"/notes/"
+export const USER_URL = API_BASE_URL+"/users/"
 export const CLOUD_UPLOAD_URL = "https://api.cloudinary.com/v1_1/da9vcx3og/image/upload"
 export const BG_HOME = "https://images.pexels.com/photos/1629212/pexels-photo-1629212.jpeg?auto=compress&cs=tinysrgb&w=7680&h=4320&dpr=1"
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// Get the current directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Load environment variables from the root .env file
+dotenv.config({ path: resolve(__dirname, '..', '.env') });
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env': {
+        ...import.meta.env,
+        // eslint-disable-next-line no-undef
+        ...process.env, // Use process.env to access environment variables
+      },
+    },
+  };
+});
